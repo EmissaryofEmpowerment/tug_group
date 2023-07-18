7c0005d3dc8b58c2d8fe97e62f4be216ad7c40ab
EmissaryofEmpowerment <promoteempowerment@gmail.com>
2023-07-18 09:59:10 -0600

    By Ryan on branch Ryan: Added images and populated display.html dynamically

--- display.js
+++ display.js
@@ -1,8 +1,66 @@
 
 function main() {
-    // document.appendChild("<p>Added child element</p>")
     let content = document.getElementById("render");
-    content.innerText = "Okay";
+    const hd_sh = document.createElement("p");
+    switch (localStorage.getItem("head_shape")) {
+        case "Oval":    
+        hd_sh.innerHTML = "<img src='Images/oval_head.png'/";           
+            break;
+
+        case "Triangle":    
+        hd_sh.innerHTML = "<img src='Images/triangle_head.png'/>";            
+            break;
+    
+        case "Star":    
+        hd_sh.innerHTML = "<img src='Images/star_head.png'/>";           
+        break;
+        
+        case "Circle":    
+        hd_sh.innerHTML = "<img src='Images/circeler_head.png'/>";          
+            break;
+        
+        default:
+            break;
+    }
+
+    const hd_clr = document.createElement("p");
+    hd_clr.innerText = "You selected: " + localStorage.getItem("head_color");
+
+    const bd_shp = document.createElement("p");
+    switch (localStorage.getItem("body_shape")) {
+        case "Oval":    
+        hd_sh.innerHTML = "<img src='Images/oval_body.png'/";           
+            break;
+
+        case "Triangle":    
+        hd_sh.innerHTML = "<img src='Images/triangle_body.png'/>";            
+            break;
+    
+        case "Star":    
+        hd_sh.innerHTML = "<img src='Images/star_body.png'/>";           
+        break;
+        
+        case "Circle":    
+        hd_sh.innerHTML = "<img src='Images/circeler_body.png'/>";          
+            break;
+        
+        default:
+            break;
+    }
+
+    const bd_clr = document.createElement("p");
+    bd_clr.innerText = "You selected: " + localStorage.getItem("body_color");
+
+    const extra = document.createElement("p");
+    extra.innerText = "You selected: " + localStorage.getItem("xtra");
+
+    const node = document.createElement("button");
+    node.innerHTML += "<a href='index.html'>Draw New Monster</a>";
+    //content.appendChild(hd_sh);
+    let node_list = [hd_sh, hd_clr, bd_shp, bd_clr, extra,node];
+    for (let index = 0; index < node_list.length; index++) {
+        content.appendChild(node_list[index]);
+                }
 }
 
 window.document.addEventListener("DOMContentLoaded", main);
--- index.js
+++ index.js
@@ -1,16 +1,22 @@
-function CompileMonster() { //No argument needed! That's a perk of local variables.
+function SetLocalVariable(body_part) { //No argument needed! That's a perk of local variables.
     //window.document.body.appendChild("<p>You clicked a radio button!</p>");
-    let block = document.getElementById("head_shape");
+    let block = document.getElementById(body_part);
     let elementCount = block.childElementCount;
     for (let index = 0; index < elementCount; index++) {
-        //block.appendChild(block[index]);
+        //block.appendChild(block.children[index]);
         let slctn = block.children[index];
         // let elem_message = "slctn["+index+"] nodeName: " + slctn.nodeName + "\n nodeType: " + slctn.nodeType + "\nnodeValue: " + slctn.nodeValue + "\nText of child node is: " + slctn.innerHTML + " ";
         // const node = document.createElement("p");
         // const node_text = document.createTextNode(elem_message);
         // node.appendChild(node_text);
         // block.appendChild(node);
-        slctn.nodeName == "INPUT" ? alert("This radio button is: " + slctn.checked):'';
+        slctn.nodeName == "INPUT" ? slctn.checked == true ? localStorage.setItem("body_part", block.children[index + 1].nodeValue) : '' :'';
+        if (slctn.nodeName == "INPUT"){
+            if (slctn.checked == true){
+                let next_value = block.children[index + 1].textContent;
+                localStorage.setItem(body_part, next_value);
+            }
+        }
     }
 }
 
@@ -19,8 +25,19 @@ function CompileMonster() { //No argument needed! That's a perk of local variabl
 document.addEventListener("DOMContentLoaded", main);
 
 function main() {    
-    let rspnd_radio_btns = document.getElementById("head_shape");
-    // .getElementsByTagName("head_shape");
-    rspnd_radio_btns.addEventListener("click", CompileMonster);
-    
+    let head_shape_btns = document.getElementById("head_shape");
+    head_shape_btns.addEventListener("click", () => {SetLocalVariable("head_shape")});
+
+    let head_clr_btns = document.getElementById("head_color");
+    head_clr_btns.addEventListener("click", () => {SetLocalVariable("head_color")});
+
+    let body_shape_btns = document.getElementById("body_shape");
+    body_shape_btns.addEventListener("click", () => {SetLocalVariable("body_shape")});
+
+    let body_clr_btns = document.getElementById("body_color");
+    body_clr_btns.addEventListener("click", () => {SetLocalVariable("body_color")});
+
+    let xtra_btns = document.getElementById("xtra");
+    xtra_btns.addEventListener("click", () => {SetLocalVariable("xtra")});
+
 }
