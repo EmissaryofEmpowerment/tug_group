1cedc5d41317ee96682f9950d9d4490ebb3111a7
EmissaryofEmpowerment <promoteempowerment@gmail.com>
2023-07-18 10:40:02 -0600

    By Ryan in branch Ryan: Now Each image loads correctly, although they do look very awkward

--- display.js
+++ display.js
@@ -26,22 +26,22 @@ function main() {
     const hd_clr = document.createElement("p");
     hd_clr.innerText = "You selected: " + localStorage.getItem("head_color");
 
-    const bd_shp = document.createElement("p");
+    const bd_sh = document.createElement("p");
     switch (localStorage.getItem("body_shape")) {
         case "Oval":    
-        hd_sh.innerHTML = "<img src='Images/oval_body.png'/";           
+        bd_sh.innerHTML = "<img src='Images/oval_body.png'/";           
             break;
 
         case "Triangle":    
-        hd_sh.innerHTML = "<img src='Images/triangle_body.png'/>";            
+        bd_sh.innerHTML = "<img src='Images/triangle_body.png'/>";            
             break;
     
         case "Star":    
-        hd_sh.innerHTML = "<img src='Images/star_body.png'/>";           
+        bd_sh.innerHTML = "<img src='Images/star_body.png'/>";           
         break;
         
         case "Circle":    
-        hd_sh.innerHTML = "<img src='Images/circeler_body.png'/>";          
+        bd_sh.innerHTML = "<img src='Images/circeler_body.png'/>";          
             break;
         
         default:
@@ -52,12 +52,28 @@ function main() {
     bd_clr.innerText = "You selected: " + localStorage.getItem("body_color");
 
     const extra = document.createElement("p");
-    extra.innerText = "You selected: " + localStorage.getItem("xtra");
+    switch (localStorage.getItem("xtra")) {
+        case "Arm":
+            extra.innerHTML = "<img src='Images/arm.png'/>"; 
+            break;
+        case "Horn1":
+            extra.innerHTML = "<img src='Images/horn.png'/>"; 
+            break;
+        case "Horn2":
+            extra.innerHTML = "<img src='Images/horn1.png'/>"; 
+            break;
+        case "Tail":
+            extra.innerHTML = "<img src='Images/tail.png'/>"; 
+            break;
+    
+        default:
+            break;
+    }
 
     const node = document.createElement("button");
     node.innerHTML += "<a href='index.html'>Draw New Monster</a>";
     //content.appendChild(hd_sh);
-    let node_list = [hd_sh, hd_clr, bd_shp, bd_clr, extra,node];
+    let node_list = [hd_sh, bd_sh, extra,node];
     for (let index = 0; index < node_list.length; index++) {
         content.appendChild(node_list[index]);
                 }
